a9acc856a8eccb5e6681a031e565dffb4a17c3b0
bulat nitaliev <bulatnitaliev2016@gmail.com>
2025-05-29 13:01:52 +0400

    add nginx

--- frontend/src/http/index.js
+++ frontend/src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // 'http://127.0.0.1:8000'
-export const API_URL = "http://127.0.0.1:8000";
+export const API_URL = "/api";
 
 const $api = axios.create({
   withCredentials: true,
